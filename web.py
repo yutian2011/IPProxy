81a949ca0de19b6d218bb757210fb219ab66d55e
yutian2011 <conanpzh@gmail.com>
2017-01-04 21:01:00 +0800

    web add types

--- web.py
+++ web.py
@@ -10,13 +10,14 @@ from settings import DB_FOR_IP
 
 app = Flask(__name__)
 
-
+TYPE = {0:"http",1:"https"}
 def db_find_one(num,cookie=False):
     if num < 0:
         return None
     r = redis.StrictRedis(REDIS_SERVER,REDIS_PORT,DB_FOR_IP)
     s = r.zrange(REDIS_SORT_SET_COUNTS,0,num-1)
     cookies = []
+    types = []
     if len(s) == 0:
         return None
     else:
@@ -24,23 +25,24 @@ def db_find_one(num,cookie=False):
             r.zincrby(REDIS_SORT_SET_COUNTS,s[i])
             if cookie == True:
                 cookies.append(r.get(s[i]))
-        return s,cookies
+            types.append(TYPE[r.zscore(REDIS_SORT_SET_TYPES,s[i])])
+        return s,types,cookies
 
 @app.route('/proxy/api/<int:num>', methods=['GET'])
 def get_proxy(num):
-    ret,cookies = db_find_one(num)
+    ret,types,cookies = db_find_one(num)
     if ret == None:
         return jsonify({"ret":False})
     else:
-        return jsonify({"ret":True,"proxy":ret})
+        return jsonify({"ret":True,"proxy":ret,"types":types})
 
 @app.route('/proxy/api/cookies/<int:num>', methods=['GET'])
 def get_proxy_with_cookies(num):
-    ret,cookies = db_find_one(num,True)
+    ret,types,cookies = db_find_one(num,True)
     if ret == None:
         return jsonify({"ret":False})
     else:
-        return jsonify({"ret":True,"proxy":ret,"cookies":cookies})
+        return jsonify({"ret":True,"proxy":ret,"types":types,"cookies":cookies})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=1129,debug=True)
